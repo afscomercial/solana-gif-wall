c7f744f4657611d0fef9ead370a277ea949b61c0
afscomercial <afscomercial@gmail.com>
2022-03-04 01:03:05 +0000

    gif grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,84 +2,148 @@ import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
-// Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+const TEST_GIFS = [
+	'https://c.tenor.com/6xwjsmMIAIoAAAAd/happy-happy-dog.gif',
+	'https://c.tenor.com/2rm6zUADvlgAAAAd/dog-serious.gif',
+	'https://c.tenor.com/pg1rt7GekXoAAAAC/dog-running.gif',
+	'https://c.tenor.com/lddYxN-HfdQAAAAM/please-pretty-please.gif'
+];
+
 const App = () => {
-  const [walletAddress, setWalletAddress] = useState(null);
-  
-  const checkIfWalletIsConnected = async () => {
-  try {
-    const { solana } = window;
-    if (solana) {
-      if (solana.isPhantom) {
-        console.log('Phantom wallet found!');
+	const [walletAddress, setWalletAddress] = useState(null);
+	const [inputValue, setInputValue] = useState('');
+	const [gifList, setGifList] = useState([]);
+	const [isConnected, setIsConnected] = useState(false);
 
-        const response = await solana.connect({ onlyIfTrusted: true });
-        console.log(
-          'Connected with Public Key:',
-          response.publicKey.toString()
-        );
-        setWalletAddress(response.publicKey.toString());
-      }
-    } else {
-      alert('Solana object not found! Get a Phantom Wallet 👻');
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { solana } = window;
+			if (solana) {
+				if (solana.isPhantom) {
+					setIsConnected(true);
+					console.log('Phantom wallet found!');
 
-  const connectWallet = async () => {
-  const { solana } = window;
+					const response = await solana.connect({ onlyIfTrusted: true });
+					console.log(
+						'Connected with Public Key:',
+						response.publicKey.toString()
+					);
+					setWalletAddress(response.publicKey.toString());
+				}
+			} else {
+				alert('Solana object not found! Get a Phantom Wallet 👻');
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  if (solana) {
-    const response = await solana.connect();
-    console.log('Connected with Public Key:', response.publicKey.toString());
-    setWalletAddress(response.publicKey.toString());
-  }
-};
+	const sendGif = async () => {
+		if (inputValue.length > 0) {
+			console.log('Gif link:', inputValue);
+			setGifList([...gifList, inputValue]);
+			setInputValue('');
+		} else {
+			console.log('Empty input. Try again.');
+		}
+	};
 
-  const renderNotConnectedContainer = () => (
-    <button
-      className="cta-button connect-wallet-button"
-      onClick={connectWallet}
-    >
-      Connect to Wallet
-    </button>
-  );
-  
-  useEffect(() => {
-    const onLoad = async () => {
-      await checkIfWalletIsConnected();
-    };
-    window.addEventListener('load', onLoad);
-    return () => window.removeEventListener('load', onLoad);
-  }, []);
-  
-  return (
-     <div className="App">
+	const onInputChange = (event) => {
+		const { value } = event.target;
+		setInputValue(value);
+	};
+
+	const connectWallet = async () => {
+		const { solana } = window;
+		if (solana) {
+			const response = await solana.connect();
+			console.log('Connected with Public Key:', response.publicKey.toString());
+			setWalletAddress(response.publicKey.toString());
+		}
+	};
+
+	const renderConnectedContainer = () => (
+		<div className="connected-container">
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+					sendGif();
+				}}
+			>
+				<input
+					type="text"
+					placeholder="Enter gif link!"
+					value={inputValue}
+					onChange={onInputChange}
+				/>
+				<button type="submit" className="cta-button submit-gif-button">
+					Submit
+				</button>
+			</form>
+			<div className="gif-grid">
+				{gifList.map((gif) => (
+					<div className="gif-item" key={gif}>
+						<img src={gif} alt={gif} />
+					</div>
+				))}
+			</div>
+		</div>
+	);
+
+	const renderNotConnectedContainer = () => (
+		<button
+			className="cta-button connect-wallet-button"
+			onClick={connectWallet}
+		>
+			Connect to Wallet
+		</button>
+	);
+
+	useEffect(() => {
+		const onLoad = async () => {
+			await checkIfWalletIsConnected();
+		};
+		window.addEventListener('load', onLoad);
+		return () => window.removeEventListener('load', onLoad);
+	}, []);
+
+	useEffect(() => {
+		if (walletAddress) {
+			console.log('Fetching GIF list...');
+
+			setGifList(TEST_GIFS);
+		}
+	}, [walletAddress]);
+
+	return (
+		<div className="App">
 			<div className={walletAddress ? 'authed-container' : 'container'}>
-        <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
-          </p>
-          {!walletAddress && renderNotConnectedContainer()}
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="header-container">
+					<p className="header"> &#128054; GIF Portal</p>
+					<p className="sub-text">
+						View your GIF collection in the metaverse ✨
+					</p>
+					{!isConnected && (
+						<p>Solana object not found! Get a Phantom Wallet 👻</p>
+					)}
+					{!walletAddress && isConnected && renderNotConnectedContainer()}
+					{walletAddress && isConnected && renderConnectedContainer()}ﬁ
+				</div>
+				<div className="footer-container">
+					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+					<a
+						className="footer-text"
+						href={TWITTER_LINK}
+						target="_blank"
+						rel="noreferrer"
+					>{`built on @${TWITTER_HANDLE}`}</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
